d73711acdde5f008bb2de8e402d98227ed1d0ce8
Maryna Zeinalova <shmapetto@gmail.com>
2023-03-09 11:22:20 +0200

    Added functions with clear names

--- domOperations.js
+++ domOperations.js
@@ -1,19 +1,42 @@
-function showResultInDomKvadrat(){
-    const side = document.getElementById('dlinaStoroniKvadrata').value;    
-    document.getElementById('ploschadKvadrata').innerHTML = getSquare(side, side);
+function showResultInDomKvadrat() {
+  const squareSide = document.getElementById("dlinaStoroniKvadrata").value;
+  const squareAreaHTML = document.getElementById("ploschadKvadrata");
+  if (squareSide > 0) {
+    squareAreaHTML.innerHTML = calculateSquare(squareSide);
+  } else {
+    squareAreaHTML.innerHTML = "Enter a positive number";
+  }
 }
-function showResultInDomPriamougolnika(){
-    const side1 = document.getElementById('pervayaStoronaPriamougolnika').value;    
-    const side2 = document.getElementById('drugayaStoronaPriamougolnika').value;    
-    document.getElementById('ploschadPriamougolnika').innerHTML = getSquare(side1, side2);
+
+function showResultInDomPriamougolnika() {
+  const side1 = document.getElementById("pervayaStoronaPriamougolnika").value;
+  const side2 = document.getElementById("drugayaStoronaPriamougolnika").value;
+  const squareRectangleHTML = document.getElementById("ploschadPriamougolnika");
+  if (side1 && side2 > 0) {
+    squareRectangleHTML.innerHTML = calculateRectangleSquare(side1, side2);
+  } else {
+    squareAreaHTML.innerHTML = "Enter a positive number";
+  }
 }
-function showResultInDomParalelograma(){
-    const side1 = document.getElementById('dlinaStoronyParalelograma').value;    
-    const side2 = document.getElementById('dlinaVisotyParalelograma').value;    
-    document.getElementById('ploschadParalelograma').innerHTML = getSquare(side1, side2);    
+
+function showResultInDomParalelograma() {
+  const side = document.getElementById("dlinaStoronyParalelograma").value;
+  const height = document.getElementById("dlinaVisotyParalelograma").value;
+  const squareParallelogramHTML = document.getElementById("ploschadParalelograma");
+  if (side && height > 0) {
+    squareParallelogramHTML.innerHTML = calculateParallelogramSquare(side, height);
+  } else {
+    squareParallelogramHTML.innerHTML = "Enter a positive number";
+  }
 }
-function showResultInDomRomba(){
-    const side1 = document.getElementById('dlinaStoronyRomba').value;    
-    const side2 = document.getElementById('dlinaVisotyRomba').value;    
-    document.getElementById('ploschadRomba').innerHTML = getSquare(side1, side2);
+
+function showResultInDomRomba() {
+  const side = document.getElementById("dlinaStoronyRomba").value;
+  const height = document.getElementById("dlinaVisotyRomba").value;
+  const squareLozengeHTML = document.getElementById("ploschadRomba");
+  if (side && height > 0) {
+    squareLozengeHTML.innerHTML = calculateLozengeSquare(side, height);
+  } else {
+    squareLozengeHTML.innerHTML = "Enter a positive number";
+  }
 }
--- libraryFormula.js
+++ libraryFormula.js
@@ -1,3 +1,19 @@
-function getSquare(side1, side2){
-    return side1*side2;
+function multiplyTwoNumbers(firstNumber, secondNumber) {
+  return firstNumber * secondNumber;
+}
+
+function calculateSquare(squareSide) {
+  return multiplyTwoNumbers(squareSide, squareSide);
+}
+
+function calculateRectangleSquare(firstRectangleSide, secondRectangleSide) {
+  return multiplyTwoNumbers(firstRectangleSide, secondRectangleSide);
+}
+
+function calculateParallelogramSquare(parellelogramSide, parallelogramHeight) {
+  return multiplyTwoNumbers(parellelogramSide, parallelogramHeight);
+}
+
+function calculateLozengeSquare(lozengeSide, lozengeHeight) {
+  return multiplyTwoNumbers(lozengeSide, lozengeHeight);
 }
